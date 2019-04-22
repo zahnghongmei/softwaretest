9b1db86dd1558dc02ef71158d283526498499046
zhanghongmei <2458457544@qq.com>
2019-04-22 22:59:04 +0800

    commit from deskTop at 2019/04/22 22:58

--- 00/Action.c
+++ 00/Action.c
@@ -1,6 +1,9 @@
 Action()
 {
 	char token1[36];
+	int i;
+	char a[2];
+	int b;
 	web_set_sockets_option("SSL_VERSION", "TLS1.1");
 	lr_start_transaction("login index");
 	web_url("web_url",
@@ -51,6 +54,7 @@ Action()
 		"Snapshot=t16.inf", 
 		"Mode=HTML", 
 		LAST);
+    //选择企业登陆后更新用户信息
     web_custom_request("updateuser",
 		"URL= https://g.t.dacube.cn/MESG-ADMIN/User/updateUser ", 
 		"Method=POST", 
@@ -60,9 +64,14 @@ Action()
 		"Snapshot=t17.inf", 
 		"Mode=HTML", 
 		"EncType=application/json;charset=UTF-8", 
-		"Body={\"compid\":\"1\",\"createdata\":\"1541494436000\",\"easemobname\":\"115201811061611139ftrmgurx\",\"email\":\"{email}\",\"headimg\":\"https://oss.dacube.cn/mesg-admin/34e0a7e9-4e3a-4afc-9c93-75b24cd55660/u=3695931498,2381510501&fm=26&gp=0.jpg\",\"isuse\":\"1\",\"phone\":\"{account}\",\"token\":\"{Token}\",\"updatedata\":\"1541494436000\",\"usercode\":\"{Usercode}\",\"username\":\"{username}\"}",LAST);
+		"Body={\"compid\":\"{comid}\",\"createdata\":\"1541494436000\",\"easemobname\":\"115201811061611139ftrmgurx\",\"email\":\"{email}\",\"headimg\":\"https://oss.dacube.cn/mesg-admin/34e0a7e9-4e3a-4afc-9c93-75b24cd55660/u=3695931498,2381510501&fm=26&gp=0.jpg\",\"isuse\":\"1\",\"phone\":\"{account}\",\"token\":\"{Token}\",\"updatedata\":\"1541494436000\",\"usercode\":\"{Usercode}\"}",
+		LAST);
 
 //登陆全媒体资源库app
+    web_reg_save_param("Mrp_link",
+		"LB=\"redirect:",
+		"RB=\"",
+		LAST);
  	web_custom_request("loginapp", 
 		"URL= https://g.t.dacube.cn/MESG-ADMIN/Application/user/appLogin", 
 		"Method=POST", 
@@ -72,7 +81,53 @@ Action()
 		"Snapshot=t18.inf", 
 		"Mode=HTML", 
 		"EncType=application/json;charset=UTF-8", 
-		"Body={\"appid\":\"1002\",\"compid\":\"1\",\"loginType\":\"pc\",\"styleType\":\"\",\"usercode\":\"{Usercode}\"}",
-		LAST);    
-	return 0;
+		"Body={\"appid\":\"1002\",\"compid\":\"{comid}\",\"loginType\":\"pc\",\"styleType\":\"\",\"usercode\":\"{Usercode}\"}",
+		LAST);
+	lr_output_message("mrp链接为：%s", lr_eval_string("{Mrp_link}"));
+	web_url("open_mrp_web",
+		"URL=https://mrp.t.dacube.cn/?token={Token}#/",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Snapshot=t19.inf", 
+		LAST);
+
+	
+	for(i=0;i<3;i++)
+	{
+	b=itoa(i,a,10);	
+	lr_save_string(a, "a1");
+   web_custom_request("createfolder", 
+		"URL=https://g.t.dacube.cn/MRP-SERVICE/mrp/v1/folder/add_folder", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://center.t.dacube.cn/", 
+		"Snapshot=t20.inf", 
+		"Mode=HTML", 
+		"EncType=application/json;charset=UTF-8", 
+		"Body={\"orgId\":\"1\",\"params\":{\"folder_detail\":\"\",\"folder_name\":\"test-{a1}\",\"id_parent\": null,\"id_repo\":\"repository-personal-1-article-e729e146-f249-42e1-960d-5c1f3f60bc9a\"},\"seed\":\"seed\",\"token\":\"{Token}\",\"userId\":\"{Usercode}\"}",
+		LAST);
+	}
+	
+	//新增一篇稿件
+	web_custom_request("add_article", 
+		"URL= https://g.t.dacube.cn/MRP-SERVICE/mrp/v1/article/add_one", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://center.t.dacube.cn/", 
+		"Snapshot=t21.inf", 
+		"Mode=HTML", 
+		"EncType=application/json;charset=UTF-8", 
+		"Body={\"orgId\":\"1\",\"params\":{\"afinal\": false,\"author\":\"123\",\"content\":\"123<div>123</div>\",\"id_folder\":\"default-repository-personal-1-article-e729e146-f249-42e1-960d-5c1f3f60bc9a\",\"id_repo\":\"repository-personal-1-article-e729e146-f249-42e1-960d-5c1f3f60bc9a\",\"source\":\"11111123\",\"summary\":\"123123.\",\"time_create\":\"15026554545\",\"time_delete\":\"15026554545\",\"time_publish\":\"1555915777000\",\"time_update\":\"1502655454\",\"title\":\"456\",\"update_type\": false},\"seed\":\"seed\",\"token\":\"{Token}\",\"userId\":\"{Usercode}\"}",
+		LAST);
+	
+  
+	
+ 
+
+
+			
+   return 0;
 }
--- 00/pre_cci.c
+++ 00/pre_cci.c
@@ -2402,6 +2402,9 @@ vuser_init()
 Action()
 {
 	char token1[36];
+	int i;
+	char a[2];
+	int b;
 	web_set_sockets_option("SSL_VERSION", "TLS1.1");
 	lr_start_transaction("login index");
 	web_url("web_url",
@@ -2452,6 +2455,7 @@ Action()
 		"Snapshot=t16.inf", 
 		"Mode=HTML", 
 		"LAST");
+     
     web_custom_request("updateuser",
 		"URL= https://g.t.dacube.cn/MESG-ADMIN/User/updateUser ", 
 		"Method=POST", 
@@ -2461,9 +2465,14 @@ Action()
 		"Snapshot=t17.inf", 
 		"Mode=HTML", 
 		"EncType=application/json;charset=UTF-8", 
-		"Body={\"compid\":\"1\",\"createdata\":\"1541494436000\",\"easemobname\":\"115201811061611139ftrmgurx\",\"email\":\"{email}\",\"headimg\":\"https://oss.dacube.cn/mesg-admin/34e0a7e9-4e3a-4afc-9c93-75b24cd55660/u=3695931498,2381510501&fm=26&gp=0.jpg\",\"isuse\":\"1\",\"phone\":\"{account}\",\"token\":\"{Token}\",\"updatedata\":\"1541494436000\",\"usercode\":\"{Usercode}\",\"username\":\"{username}\"}","LAST");
+		"Body={\"compid\":\"{comid}\",\"createdata\":\"1541494436000\",\"easemobname\":\"115201811061611139ftrmgurx\",\"email\":\"{email}\",\"headimg\":\"https://oss.dacube.cn/mesg-admin/34e0a7e9-4e3a-4afc-9c93-75b24cd55660/u=3695931498,2381510501&fm=26&gp=0.jpg\",\"isuse\":\"1\",\"phone\":\"{account}\",\"token\":\"{Token}\",\"updatedata\":\"1541494436000\",\"usercode\":\"{Usercode}\"}",
+		"LAST");
 
  
+    web_reg_save_param("Mrp_link",
+		"LB=\"redirect:",
+		"RB=\"",
+		"LAST");
  	web_custom_request("loginapp", 
 		"URL= https://g.t.dacube.cn/MESG-ADMIN/Application/user/appLogin", 
 		"Method=POST", 
@@ -2473,9 +2482,55 @@ Action()
 		"Snapshot=t18.inf", 
 		"Mode=HTML", 
 		"EncType=application/json;charset=UTF-8", 
-		"Body={\"appid\":\"1002\",\"compid\":\"1\",\"loginType\":\"pc\",\"styleType\":\"\",\"usercode\":\"{Usercode}\"}",
-		"LAST");    
-	return 0;
+		"Body={\"appid\":\"1002\",\"compid\":\"{comid}\",\"loginType\":\"pc\",\"styleType\":\"\",\"usercode\":\"{Usercode}\"}",
+		"LAST");
+	lr_output_message("mrpÁ´½ÓÎª£º%s", lr_eval_string("{Mrp_link}"));
+	web_url("open_mrp_web",
+		"URL=https://mrp.t.dacube.cn/?token={Token}#/",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Snapshot=t19.inf", 
+		"LAST");
+
+	
+	for(i=0;i<3;i++)
+	{
+	b=itoa(i,a,10);	
+	lr_save_string(a, "a1");
+   web_custom_request("createfolder", 
+		"URL=https://g.t.dacube.cn/MRP-SERVICE/mrp/v1/folder/add_folder", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://center.t.dacube.cn/", 
+		"Snapshot=t20.inf", 
+		"Mode=HTML", 
+		"EncType=application/json;charset=UTF-8", 
+		"Body={\"orgId\":\"1\",\"params\":{\"folder_detail\":\"\",\"folder_name\":\"test-{a1}\",\"id_parent\": null,\"id_repo\":\"repository-personal-1-article-e729e146-f249-42e1-960d-5c1f3f60bc9a\"},\"seed\":\"seed\",\"token\":\"{Token}\",\"userId\":\"{Usercode}\"}",
+		"LAST");
+	}
+	
+	 
+	web_custom_request("add_article", 
+		"URL= https://g.t.dacube.cn/MRP-SERVICE/mrp/v1/article/add_one", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=https://center.t.dacube.cn/", 
+		"Snapshot=t21.inf", 
+		"Mode=HTML", 
+		"EncType=application/json;charset=UTF-8", 
+		"Body={\"orgId\":\"1\",\"params\":{\"afinal\": false,\"author\":\"123\",\"content\":\"123<div>123</div>\",\"id_folder\":\"default-repository-personal-1-article-e729e146-f249-42e1-960d-5c1f3f60bc9a\",\"id_repo\":\"repository-personal-1-article-e729e146-f249-42e1-960d-5c1f3f60bc9a\",\"source\":\"11111123\",\"summary\":\"123123.\",\"time_create\":\"15026554545\",\"time_delete\":\"15026554545\",\"time_publish\":\"1555915777000\",\"time_update\":\"1502655454\",\"title\":\"456\",\"update_type\": false},\"seed\":\"seed\",\"token\":\"{Token}\",\"userId\":\"{Usercode}\"}",
+		"LAST");
+	
+  
+	
+ 
+
+
+			
+   return 0;
 }
 # 5 "e:\\test\\00\\\\combined_00.c" 2
 
